85e79070164caf30c31d62717d5d4264e64ac4d8
sidneyd3vries <sidney.de.vries@hotmail.com>
2016-12-09 13:37:39 +0100

    Update plot.py
    normal distribution + skewed distribution

--- plot/plot.py
+++ plot/plot.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 from collections import Counter, OrderedDict
 import numpy as np
+import sys
+
+# Get list of arguments
+arglist = sys.argv
+
+# Set arguments
+arg1 = arglist[1]
 
 # Reads list from file
 # File must have int or float, one value per line
@@ -9,8 +16,7 @@ def readfile(file):
 	output = [int(float(i.strip())) for i in temp]
 	return output
 
-# Define file
-FILE = "newvalues.txt"
+FILE = str(arg1)
 
 # Y is list from file
 x = readfile(FILE)
@@ -18,17 +24,28 @@ x = readfile(FILE)
 # Average and standard deviation
 avg = sum(x)/len(x)
 std = np.std(x)
+med = np.median(x)
 
-# Print some data
-print ("Average value: ", avg)
-print ("Standard deviation: ", std)
+# Normally distributed data
+print ("Normal distribution: ")
+print ("Average value: ", round(avg, 1))
+print ("Standard deviation: ", round(std, 0))
 print ("Approx. 68% of values between: ", int(round(avg - 1*std, -2)), " and ", int(round(avg + 1*std, -2)))
 print ("Approx. 95% of values between: ", int(round(avg - 2*std, -2)), " and ", int(round(avg + 2*std, -2)))
 print ("Approx. 99% of values between: ", int(round(avg - 2*std, -2)), " and ", int(round(avg + 3*std, -2)))
+print ("Highest found value: ", max(x))
+print ()
+
+# Skewed data
+print ("Skewed distribution:  ")
+print ("Median: ", med)
+print ("Better than 75% if value is higher than: ", np.percentile(x,75))
+print ("Better than 95% if value is higher than: ", np.percentile(x,95))
+print ("Better than 99% if value is higher than: ", np.percentile(x,99))
+print ("Highest found value: ", max(x))
 
 # Round values here
 y = [round(i,-5) for i in x]
-
 # Count each value
 count= Counter(y)
 
@@ -42,7 +59,7 @@ od = OrderedDict(sorted(D.items()))
 plt.bar(range(len(od)), od.values(), align='center')
 plt.xticks(range(len(od)), list(od.keys()))
 locs, labels = plt.xticks()
-plt.setp(labels, rotation=60)
+plt.setp(labels, rotation=45)
 
 # Show plot
 plt.show()
