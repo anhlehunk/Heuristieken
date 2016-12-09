4fa155a0def711d7a1bceda20a779afb0e284af1
sidneyd3vries <sidney.de.vries@hotmail.com>
2016-12-09 13:38:29 +0100

    system arguments

--- plot/mergefile.py
+++ plot/mergefile.py
@@ -1,3 +1,12 @@
+import sys
+
+arglist = sys.argv
+
+file1 = arglist[1]
+file2 = arglist[2]
+output = arglist[3]
+
+
 # Write list to file, every item on a new line
 def writefile(file, input):
 	outfile = open(file, "w")
@@ -5,18 +14,18 @@ def writefile(file, input):
 	outfile.close()
 
 # Reads list from file
-# File must have int or float, one value per line
 def readfile(file):
 	temp = open(file).readlines()
 	output = [int(float(i.strip())) for i in temp]
-	rounded = [round(i,-5) for i in output] # Rounded to 5 zeros before comma
-	return rounded
+	return output
 
 # Merge file1 and file2 to outputfile
 def merge(file1, file2, outputfile):
 	temp1 = readfile(file1)
 	temp2 = readfile(file2)
 	thelist = temp1 + temp2
-	stripped = [i.strip() for i in thelist]
-	allint = [int(float(i)) for i in stripped]
+	#stripped = [i.strip() for i in thelist]
+	allint = [int(float(i)) for i in thelist]
 	writefile(outputfile, allint)
+
+merge(file1, file2, output)
